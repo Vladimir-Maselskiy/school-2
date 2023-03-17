228681c8848c40d2909e89d8c6948030e99c4c40
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-03-17 18:54:11 +0200

    feat: add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Page } from './pages/Page/Page';
+import '../App.css';
+import { Home } from './component/Home/Home';
+import { fetchCoursesData } from './utils/fetchCoursesData';
+import { ICourse } from './interfaces/interfaces';
+import { getToken } from './utils/getToken';
+import previewCoursesSaved from './data/previewCourses.json';
+
 function App() {
+  const [courses, setCourses] = useState<ICourse[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await getToken();
+      console.log('token in app', token);
+      // await setAuthToken(token);
+      setTimeout(async () => {
+        const data = await fetchCoursesData(token);
+        console.log('data in app', data);
+
+        if (data) {
+          setCourses(data.courses);
+        } else {
+          setCourses(previewCoursesSaved.courses);
+        }
+      }, 1000);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home courses={courses} />}></Route>
+      <Route path="/:current" element={<Page />}></Route>
+    </Routes>
   );
 }
 
