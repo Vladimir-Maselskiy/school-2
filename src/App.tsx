ff30c03469b23f7ca8354fcd07c65b24c6dbaa5e
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-03-17 20:36:29 +0200

    fix: 15

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return <h1>hi</h1>;
 }
-
+<Routes>
+  <Route path="/" element={<h1>hi1</h1>}></Route>
+  <Route path="/:current" element={<h1>hi2</h1>}></Route>
+</Routes>;
 export default App;
